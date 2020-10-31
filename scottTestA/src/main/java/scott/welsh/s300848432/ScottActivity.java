03c0f34b504dd2f02dc11faf31e253bdb6b357dd
Scott Welsh <scott.welsh@hotmail.com>
2020-10-31 08:50:55 -0400

    Project Skeleton

---
+++ scottTestA/src/main/java/scott/welsh/s300848432/ScottActivity.java
@@ -0,0 +1,18 @@
+package scott.welsh.s300848432;
+
+// Scott Welsh
+// Student ID: 300848432
+//section number: 002
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScottActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scott);
+    }
+}
